0a9ef8d123f5bddef61bc048783ac80764a339c1
Julian Geiger <julian.geiger@epfl.ch>
2023-03-17 18:33:04 +0100

    Added option of showing last n nodes for gstn.

--- functions/gstn.fish
+++ functions/gstn.fish
@@ -1,9 +1,16 @@
 function gstn -w "" -d "Show pk and status of all nodes of a group."
 
-  echo "Showing all nodes for group: $argv[1]"
+  if test (count $argv) -lt 2
+    echo "Showing all nodes for group: $argv[1]"
+    set numnodes 100
+  else
+    echo "Showing last $argv[2] nodes for group: $argv[1]"
+    set numnodes $argv[2]
+  end
 
-  for group_node in $(verdi group show $argv[1] | awk '{print $1}' | egrep [0-9]{4})
+  for group_node in $(verdi group show $argv[1] | awk '{print $1}' | egrep [0-9]{4} | tail -n $numnodes)
     verdi node show $group_node | grep -B 1 'pk           ' | sort
     printf '=%.0s' (seq -w 1 25); echo
   end
+
 end
